d3be9fa25b15441cde8165b1c0824916ede5f482
Gareth-Yao <347747741@qq.com>
2020-02-23 17:18:11 -0800

    added some lru ordering

--- lstore/db.py
+++ lstore/db.py
@@ -3,15 +3,19 @@ from lstore.page import *
 from collections import defaultdict
 import os, sys, json
 
+
 class Bufferpool:
 
-    def __init__(self,db):
+    def __init__(self, db):
         self.db = db
         self.empty = [i for i in range(90)]
         self.used = []
-        self.pool = [Page("",()) for i in range(90)]                #empty page, will be replace by real page     
-        self.directory = defaultdict(lambda:-1)                      #(table, b or t, page_index, column_number) -> index in pool
-                                                                    # -1 if not in pool(need to get from file)
+        self.pinned = np.zero(90, dtype=int)
+        self.LRUIndex = np.empty(90, dtype=int)
+        self.pool = [Page("", ()) for i in range(90)]  # empty page, will be replace by real page
+        self.directory = defaultdict(lambda: -1)  # (table, b or t, page_index, column_number) -> index in pool
+        # -1 if not in pool(need to get from file)
+
     def flush_pool(self):
         for page in self.pool:
             if page.dirty:
@@ -19,24 +23,31 @@ class Bufferpool:
                 data_str = page.data
                 file.write(data_str)
                 file.close()
-    
+
     def get_from_disk(self, filename, meta):
-        if os.stat(filename).st_size >0:
-            file=open(filename,"rb")
+        if os.stat(filename).st_size > 0:
+            file = open(filename, "rb")
             data_str = file.read()
-            page = Page(filename,meta)
+            page = Page(filename, meta)
             page.data = data_str
         else:
-            page = Page(filename,meta)
-        
+            page = Page(filename, meta)
+
         page.dirty = False;
         empty_index = self.empty.pop()
         self.used.append(empty_index)
+        self.LRUIndex[empty_index] = len(self.used) - 1
         self.pool[empty_index] = page
         return empty_index
-    
+
     def evict(self):
-        evict_index = self.used.pop(0)                                #evict oldest page
+        evict_index = 0
+        for i in range(0, len(self.used)):  # Evict the least recently used and unpinned page
+            if (pinned[self.used[i]] == 0):
+                evict_index = self.used.pop(i)
+                self.LRUIndex[evict_index] = -1
+                break
+        # evict oldest page
         self.empty.append(evict_index)
         if self.pool[evict_index].dirty:
             page = self.pool[evict_index]
@@ -45,16 +56,17 @@ class Bufferpool:
             file.write(data_str)
             file.close()
         del self.directory[page.location]
-    
+
     def getindex(self, table, bt, page, column):
-        i = self.directory[(table,bt,page,column)]
+        i = self.directory[(table, bt, page, column)]
         if i == -1:
             if len(self.used) == len(self.pool):
                 self.evict()
-        path = os.getcwd() + "/" + bt + "_" + str(page) + "c_" +str(column)
-        i = self.get_from_disk(path, (table, bt, page, column))
-        self.directory.update({(table, bt, page, column): i})
+            path = os.getcwd() + "/" + bt + "_" + str(page) + "c_" + str(column)
+            i = self.get_from_disk(path, (table, bt, page, column))
+            self.directory.update({(table, bt, page, column): i})
         return i
+
 
 class Database():
 
@@ -63,11 +75,10 @@ class Database():
         self.bufferpool = Bufferpool(self)
         pass
 
-    def open(self,path):
+    def open(self, path):
         if not os.path.exist(path):
             os.makedirs(path)
         os.chdir(path)
-        
 
     def close(self):
         for table in self.tables.items():
@@ -81,21 +92,24 @@ class Database():
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
     """
+
     def create_table(self, name, num_columns, key):
-        table = Table(name,  num_columns, key, self.bufferpool)
+        table = Table(name, num_columns, key, self.bufferpool)
         self.tables.update({name: table})
         return table
 
     def get_table(self, name):
-        with open(os.getcwd()+'/'+name+'/metadata.json','r') as fp:
+        with open(os.getcwd() + '/' + name + '/metadata.json', 'r') as fp:
             meta_dict = json.load(fp.read())
         fp.close()
-        table = Table(name, meta_dict['num_column'], meta_dict['key'], self.bufferpool, meta_dict['num_basepage'], meta_dict['num_tailpage'], meta_dict['total_records'])
+        table = Table(name, meta_dict['num_column'], meta_dict['key'], self.bufferpool, meta_dict['num_basepage'],
+                      meta_dict['num_tailpage'], meta_dict['total_records'])
         return table
 
     """
     # Deletes the specified table
     """
+
     def drop_table(self, name):
         del self.tables[name]
         pass
