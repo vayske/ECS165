f45ed3078c644b86dbf92a892a79674f4c43c0f6
cjh010 <49503648+cjh010@users.noreply.github.com>
2020-03-07 15:01:28 -0800

    Update query.py

--- lstore/query.py
+++ lstore/query.py
@@ -200,7 +200,22 @@ class Query:
                     result += value
         return result
 
-
+    """
+    incremenets one column of the record
+    this implementation should work if your select and update queries already work
+    :param key: the primary of key of the record to increment
+    :param column: the column to increment
+    # Returns True is increment is successful
+    # Returns False if no record matches key or if target record is locked by 2PL.
+    """
+    def increment(self, key, column):
+        r = self.select(key, self.table.key, [1] * self.table.num_columns)[0]
+        if r is not False:
+            updated_columns = [None] * self.table.num_columns
+            updated_columns[column] = r[column] + 1
+            u = self.update(key, *updated_columns)
+            return u
+        return False
 
 
 
