016a8c35054b94e42a502908809d414f1132acc9
cjh010 <49503648+cjh010@users.noreply.github.com>
2020-03-07 16:03:05 -0800

    Create counter.py

---
+++ lstore/counter.py
@@ -0,0 +1,21 @@
+#Counter with lock, used in pin and lock dictionary
+class Couter():
+    def __init__(self, value=0):
+        self.value = value
+        self.lock = threading.Lock()
+
+    def inc(self):
+        with self.lock:
+            self.value += 1
+            return self.value
+
+    def dec(self):
+        with self.lock:
+            if self.value >0:
+                self.value -= 1
+            return self.value
+            
+    def get(self):
+      return self.value
+    
+    
